3509d49d4e027c0aa34f641f878b353195fcb2c6
xmartinly <778567144@qq.com>
2025-03-31 14:45:47 +0800

    880PRO-OFC\eet4  20250331.144547

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,4 +1,8 @@
 // export const App_Request = new AppRequest();
-export const baseUrlApi = (url: string) => `http://127.0.0.1:8000/api/${url}`;
-// export const baseUrlApi = (url: string) =>
-//   `https://order-api.infcd.com/api/${url}`;
+const local = "http://127.0.0.1:8000/api/";
+const remote = "https://order-api.infcd.com/api/";
+const is_dev = false;
+
+export const baseUrlApi = (url: string) => {
+  return is_dev ? `${local}${url}` : `${remote}${url}`;
+};
